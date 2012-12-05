bfbd519e2cc427cc310bfb1582b329bab2be55d8
Ryan <cumt.xiaochi@gmail.com>
2012-12-05 14:03:55 +0800

    add 参与�
    and
    move it to a sub directory

---
+++ wireframe/wiki_entry_files/axurerp_pagespecificscript.js
@@ -0,0 +1,62 @@
+﻿for(var i = 0; i < 15; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u4'] = 'top';document.getElementById('u0_img').tabIndex = 0;
+
+u0.style.cursor = 'pointer';
+$axure.eventManager.click('u0', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('index.html');
+
+}
+});
+u8.tabIndex = 0;
+
+u8.style.cursor = 'pointer';
+$axure.eventManager.click('u8', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('user.html');
+
+}
+});
+gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u9'] = 'top';u14.tabIndex = 0;
+
+u14.style.cursor = 'pointer';
+$axure.eventManager.click('u14', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('user.html');
+
+}
+});
+gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u6'] = 'center';u2.tabIndex = 0;
+
+u2.style.cursor = 'pointer';
+$axure.eventManager.click('u2', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('login.html');
+
+}
+});
+gv_vAlignTable['u2'] = 'top';u13.tabIndex = 0;
+
+u13.style.cursor = 'pointer';
+$axure.eventManager.click('u13', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('user.html');
+
+}
+});
+gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u7'] = 'top';
