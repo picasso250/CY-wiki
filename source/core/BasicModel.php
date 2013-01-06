d49c38f651b1d78ee9723880892def0476247259
Ryan <cumt.xiaochi@gmail.com>
2013-01-06 11:19:03 +0800

    add Searcher

--- source/core/BasicModel.php
+++ source/core/BasicModel.php
@@ -4,14 +4,12 @@
  * @author ryan
  */
 
-// get_called_class() can be replaced by static::
-
 class BasicModel
 {
     protected $id = null;
     protected $info = null;
     
-    public function __construct($para) 
+    public function __construct($para)
     {
         if (is_array($para) && isset($para['id'])) {
             $this->id = $para['id'];
@@ -29,7 +27,7 @@ class BasicModel
     public static function create($info = array())
     {
         $self = get_called_class();
-        Pdb::insert($info, $self::table());
+        Pdb::insert($info, $self::$table);
         return new $self(Pdb::lastInsertId());
     }
 
@@ -45,18 +43,9 @@ class BasicModel
         return Pdb::count($tables, $conds);
     }
 
-    public static function read($conds = array())
+    public function toArray()
     {
-        $self = get_called_class();
-        $arr = $self::buildDbArgs($conds);
-        if (count($arr) != 4) {
-            throw new Exception("build db arg, not 4", 1);
-        }
-        list($tables, $conds, $orderby, $tail) = $arr;
-        $infos = Pdb::fetchAll('*', $tables, $conds, $orderby, $tail);
-        return array_map(function ($info) use($self) {
-            return new $self($info);
-        }, $infos);
+        return $this->info();
     }
 
     public function info() // will this bug?
@@ -71,7 +60,7 @@ class BasicModel
 
     public function exists()
     {
-        return Pdb::exists(self::table(), $this->selfCond());
+        return Pdb::exists(static::table(), $this->selfCond());
     }
 
     public function selfCond()
@@ -85,7 +74,7 @@ class BasicModel
         if (isset($self::$table))
             return $self::$table;
         else 
-            return strtolower($self);
+            return camel2under($self); // camal to underscore
     }
 
     public function update($key_or_array, $value = null)
@@ -128,7 +117,6 @@ class BasicModel
             return new $class($this->info[$prop]);
         } else {
             throw new Exception("no $prop", 1);
-            
         }
     }
 
@@ -137,12 +125,8 @@ class BasicModel
         Pdb::del(self::table(), $this->selfCond());
     }
 
-    protected static function buildDbArgs($conds = array())
+    public static function search()
     {
-        $tables = self::table();
-        $conds = array();
-        $orderby = array();
-        $tail = '';
-        return array($tables, $conds, $orderby, $tail);
+        return new Searcher(get_called_class());
     }
 }
