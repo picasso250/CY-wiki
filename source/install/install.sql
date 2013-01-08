f0e71e1903048f0f74f778ce10f3941d8e33aabe
Ryan <cumt.xiaochi@gmail.com>
2013-01-08 12:36:20 +0800

    db

--- source/install/install.sql
+++ source/install/install.sql
@@ -11,14 +11,15 @@ USE `cywiki`;
 -- 表的结构 `entry`
 --
 
-CREATE TABLE `entry` (
+CREATE TABLE IF NOT EXISTS `entry` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `creator` int(10) unsigned NOT NULL,
   `title` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
   `latest` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -26,14 +27,14 @@ CREATE TABLE `entry` (
 -- 表的结构 `user`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` char(50) COLLATE utf8_unicode_ci NOT NULL,
   `password` char(64) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
@@ -41,7 +42,7 @@ CREATE TABLE `user` (
 -- 表的结构 `version`
 --
 
-CREATE TABLE `version` (
+CREATE TABLE IF NOT EXISTS `version` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `entry` int(10) unsigned NOT NULL,
   `editor` int(10) unsigned NOT NULL,
@@ -49,7 +50,18 @@ CREATE TABLE `version` (
   `reason` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `edited` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10 ;
 
---------------------------------------------------
-ALTER TABLE  `entry` ADD  `creator` INT( 10 ) UNSIGNED NOT NULL AFTER  `id`;
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `watch`
+--
+
+CREATE TABLE IF NOT EXISTS `watch` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user` int(10) unsigned NOT NULL,
+  `entry` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `u_e` (`user`,`entry`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
