0a05ac3111fd7516f8feab8d3565f6b6830c59c6
picasso250 <cumt.xiaochi@gmail.com>
2013-04-13 22:55:00 +0800

    back

--- source/controller/index.php
+++ source/controller/index.php
@@ -9,19 +9,6 @@ class IndexController extends BasicController {
     {
         parent::__construct();
 
-
-        $versions = Version::search()->find();
-        foreach ($versions as $key => $v) {
-            $e = $v->entry();
-            if ($e->exists()) {
-                $e = $e->toArray();
-                if (isset($e['title'])) {
-                    $v->title = $e['title'];
-                }
-            }
-        }
-        echo "ok";
-        exit;
         $recents = Entry::recents(10);
         render_view('master', compact('recents'));
     }
